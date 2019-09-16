b51f76d7f210d76a914742961da1d73f658f9530
longshun <2668941683@qq.com>
2019-09-16 15:43:00 +0800

    删除test类，对主程序没有影响

--- src/main/java/com/mysiteforme/admin/dao/EtaskDao.java
+++
@@ -1,9 +0,0 @@
-package com.mysiteforme.admin.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EtaskDao {
-
-   public void EtaskAll();
-}
--- src/main/java/com/mysiteforme/admin/service/EtaskService.java
+++
@@ -1,5 +0,0 @@
-package com.mysiteforme.admin.service;
-
-public interface EtaskService {
-    public void etask();
-}
